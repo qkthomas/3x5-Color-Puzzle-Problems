1a3d987206539cd95403fff7f8fa90466044f1b8
Lin Chen <qkthomas@gmail.com>
2014-01-24 23:23:50 -0500

    refine Chip's fields. Organize position as an array. Finish DrawMyself() method.

--- 16ColorsPuzzle/Chip.cs
+++ 16ColorsPuzzle/Chip.cs
@@ -11,19 +11,16 @@ namespace _16ColorsPuzzle
     class Chip
     {
         private Color chip_color;
-        private Tuple<int, int, char> chip_position_x_y_char;
+        private Tuple<int, char> position_by_number_and_letter;
 
         //record the grid config for self-drawing
-        private Tuple<int, int> grid_m_n_config;
+        private Tuple<int, int> row_column_config;
 
-        private int edge;
-
-        public Chip(Color IN_color, Tuple<int, int, char> IN_position, int IN_edge, Tuple<int, int> IN_grid_config)
+        public Chip(Color IN_color, Tuple<int, char> IN_position, Tuple<int, int> IN_grid_config)
         {
             this.chip_color = IN_color;
-            this.chip_position_x_y_char = IN_position;
-            this.edge = IN_edge;
-            this.grid_m_n_config = IN_grid_config;
+            this.position_by_number_and_letter = IN_position;
+            this.row_column_config = IN_grid_config;
         }
 
         public void DrawMyself(Panel p)
@@ -31,11 +28,21 @@ namespace _16ColorsPuzzle
             using (Graphics g = p.CreateGraphics())
             {
                 Brush brush = new SolidBrush(this.chip_color);
+                Pen pen = new Pen(Color.Black);
                 //attain chip drawing size according to the size of the Panel.
-                Rectangle rtg = new Rectangle(new Point(p.Size.Height / grid_m_n_config.Item1 * chip_position_x_y_char.Item1, p.Size.Width / grid_m_n_config.Item2 * chip_position_x_y_char.Item2), new Size(p.Size.Height / grid_m_n_config.Item1, p.Size.Width / grid_m_n_config.Item2));
+                int column_number = this.position_by_number_and_letter.Item1 % this.row_column_config.Item2;
+                int row_number = this.position_by_number_and_letter.Item1 / this.row_column_config.Item2;
+                Point rtg_upperleft_point = new Point(p.Size.Width / this.row_column_config.Item2 * column_number, p.Size.Height / this.row_column_config.Item1 * row_number);
+                Size rtg_size = new Size(p.Size.Width / this.row_column_config.Item2, p.Size.Height / this.row_column_config.Item1);
+                Rectangle rtg = new Rectangle(rtg_upperleft_point, rtg_size);
                 g.FillRectangle(brush, rtg);
+                g.DrawRectangle(pen, rtg);
+                if(Color.Transparent == this.chip_color)
+                {
+                    g.DrawLine(pen, new Point(rtg.X, rtg.Y), new Point(rtg.X + rtg.Width, rtg.Y + rtg.Height));
+                    g.DrawLine(pen, new Point(rtg.X + rtg.Width, rtg.Y), new Point(rtg.X, rtg.Y + rtg.Height));
+                }
             }
-
         }
     }
 }
