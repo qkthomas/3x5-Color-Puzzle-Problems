b0116d230ce198f2d6e7d51d225bed8c944e16ff
Lin Chen <qkthomas@gmail.com>
2014-01-24 23:27:09 -0500

    Added ChipsList.cs, an extended version of List<Chip>

---
+++ 16ColorsPuzzle/ChipsList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16ColorsPuzzle
+{
+    class ChipsList : List<Chip>
+    {
+        public void SwapTwoChips(int index1, int index2)
+        {
+            Chip chip_at_index1 = base[index1];
+            Chip chip_at_index2 = base[index2];
+            base[index1] = chip_at_index2;
+            base[index2] = chip_at_index1;
+        }
+    }
+}
