92da11196224c96f67b9765a2ff65ef1522fd4ea
Lin Chen <qkthomas@gmail.com>
2014-01-24 23:24:36 -0500

    finish openConfigurationToolStripMenuItem_Click() function. draw the input configuration right after opening the input text file.

--- 16ColorsPuzzle/Form1.cs
+++ 16ColorsPuzzle/Form1.cs
@@ -13,11 +13,13 @@ namespace _16ColorsPuzzle
     public partial class Form1 : Form
     {
         Form_Debug fd;
+        ChipsList cplst;
         public Form1()
         {
             InitializeComponent();
             this.fd = new Form_Debug();
             fd.Show();
+            cplst = new ChipsList();
         }
 
         private void openConfigurationToolStripMenuItem_Click(object sender, EventArgs e)
@@ -25,11 +27,23 @@ namespace _16ColorsPuzzle
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Title = @"Please select a configuration text file";
             ofd.Filter = @"Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            ofd.FilterIndex = 2;
+            ofd.FilterIndex = 1;
+
+            #region debug code
             if (DialogResult.OK == ofd.ShowDialog())
             {
                 fd.debug_text.AppendText(ofd.FileName + "\r\n");
             }
+            #endregion
+
+            string[] inputed_str = PzzConfigFileReader.ReadAsArray(ofd.FileName);
+            this.cplst = PzzConfigFileReader.BuildChipsConfiguration(Tuple.Create<int, int>(3, 5), inputed_str);
+
+            foreach(var cp in cplst)
+            {
+                cp.DrawMyself(this.panel1);
+            }
+
         }
     }
 }
