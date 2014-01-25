af4af5bb2888cf0d6792eb5ff91c4adfa8110edd
Lin Chen <qkthomas@gmail.com>
2014-01-24 23:26:08 -0500

    finish ConvertIndexOfArrayToPositionOfBoard() and BuildChipsConfiguration() method. However, the way of attaining index from the inputed string[] is not good. It should be done better.

--- 16ColorsPuzzle/PzzConfigFileReader.cs
+++ 16ColorsPuzzle/PzzConfigFileReader.cs
@@ -11,40 +11,54 @@ namespace _16ColorsPuzzle
 {
     class PzzConfigFileReader
     {
-        private Dictionary<string, Color> dict_string_color = new Dictionary<string, Color>();
+        static private Dictionary<string, Color> dict_string_color = new Dictionary<string, Color>();
 
-        public PzzConfigFileReader()
+        static PzzConfigFileReader()
         {
             //hardcoded defined colors. It should be done better.
-            this.dict_string_color.Add("r", Color.Red);
-            this.dict_string_color.Add("b", Color.Blue);
-            this.dict_string_color.Add("w", Color.White);
-            this.dict_string_color.Add("y", Color.Yellow);
-            this.dict_string_color.Add("g", Color.Green);
-            this.dict_string_color.Add("p", Color.Pink);
-            this.dict_string_color.Add("e", Color.Transparent);
+            dict_string_color.Add("r", Color.Red);
+            dict_string_color.Add("b", Color.Blue);
+            dict_string_color.Add("w", Color.White);
+            dict_string_color.Add("y", Color.Yellow);
+            dict_string_color.Add("g", Color.Green);
+            dict_string_color.Add("p", Color.Pink);
+            dict_string_color.Add("e", Color.Transparent);
         }
 
-        static string[] ReadAsArray(string filename)
+        public static string[] ReadAsArray(string filename)
         {
+            string[] lineArrary = null;
             using (StreamReader sr = new StreamReader(filename))
             {
                 string line = null;
 
                 if((line = sr.ReadLine()) != null)
                 {
-                    string[] lineArrary = Regex.Split(line, " ", RegexOptions.IgnoreCase);
+                    lineArrary = Regex.Split(line, " ", RegexOptions.IgnoreCase);
                 }
             }
-            return null;
+            return lineArrary;
         }
 
-        static Tuple<int, int, char> ConvertIndexOfArrayToPositionOfBoard(int IN_position_in_array, int IN_length_of_a_row)
+        private static char ConvertIndexOfArrayToPositionOfBoard(int IN_position_in_array)
         {
-            int row_number = IN_position_in_array / IN_length_of_a_row;
-            int column_number = IN_position_in_array % IN_length_of_a_row;
-            char position_representing_char = (char)(IN_position_in_array + 0x0041);
-            return Tuple.Create<int, int, char>(item1 : row_number, item2 : column_number, item3 : position_representing_char);
+            char position_representing_char = (char)(IN_position_in_array + 0x0041); //0x0041 refer to 'A' in Unicode
+            return position_representing_char;
+        }
+
+        public static ChipsList BuildChipsConfiguration(Tuple<int, int> IN_row_column_config, string[] IN_inputed_str_array)
+        {
+            ChipsList cplst = new ChipsList();
+            List<string> strlst = IN_inputed_str_array.OfType<string>().ToList();
+            int index = 0;
+            foreach (var str in IN_inputed_str_array)
+            {
+                Color color = dict_string_color[str];
+                Tuple<int, char> chip_position = Tuple.Create<int, char>(index, ConvertIndexOfArrayToPositionOfBoard(index));
+                cplst.Add(new Chip(color, chip_position, IN_row_column_config));
+                index++;
+            }
+            return cplst;
         }
     }
 }
