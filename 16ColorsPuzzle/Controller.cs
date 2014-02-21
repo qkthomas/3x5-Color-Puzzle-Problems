73a38b52040fa3758ffcf6bd5c063b5a0f7a1f09
Lin Chen <qkthomas@gmail.com>
2014-02-21 13:58:36 -0500

    added logger and goal state checking

--- 16ColorsPuzzle/Controller.cs
+++ 16ColorsPuzzle/Controller.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.IO;
 
 namespace _16ColorsPuzzle
 {
@@ -12,13 +13,67 @@ namespace _16ColorsPuzzle
         private Tuple<int, int> mRowColumnConfig;
         private ChipsList mChipsList;
         private int mCurrentEmptySpaceIndex;
+        private Logger mLogger = new Logger();
 
-        enum Directions { up, down, left, right }
+        enum Directions { up, down, left, right };
+
+        public class Logger
+        {
+            private List<char> mListMovements;
+            private int mLogStartPosition = 1;
+            private int mNewLineEveryNRecord = 5;
+            private string mLogFileName = "log.txt";
+            private char mFirstPositionRepresentChar = 'A';
+
+            public Logger()
+            {
+                mListMovements = new List<char>();
+            }
+
+            public void AddMove(int index_in_chips_list)
+            {
+                int position_number_unicode = (int)this.mFirstPositionRepresentChar + index_in_chips_list;
+                char move = (char)position_number_unicode;
+                this.mListMovements.Add(move);
+            }
+
+            public void WriteLogToDisk()
+            {
+                FileStream fs = new FileStream(this.mLogFileName, FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+                StringBuilder sb = new StringBuilder();
+                int number_step = 1;
+                foreach(char c in this.mListMovements)
+                {
+                    if (0 == this.mLogStartPosition % this.mNewLineEveryNRecord)
+                    {
+                        sb.Append(number_step.ToString() + ") ");
+                        sb.Append(c.ToString());
+                        sb.AppendLine(", ");
+                    }
+                    else
+                    {
+                        sb.Append(number_step.ToString() + ") ");
+                        sb.Append(c.ToString());
+                        sb.Append(", ");
+                    }
+                    this.mLogStartPosition++;
+                    number_step++;
+                }
+                sw.Write(sb.ToString());
+                sw.Close();
+            }
+        }
 
         public Controller(Tuple<int, int> row_column_config)
         {
             this.mRowColumnConfig = row_column_config;
             this.mChipsList = new ChipsList();
+        }
+
+        public void PrintLogger()
+        {
+            this.mLogger.WriteLogToDisk();
         }
 
         //better to use delegate, and also need refactoring
@@ -30,6 +85,7 @@ namespace _16ColorsPuzzle
             {
                 this.SwapTwoChips(this.mCurrentEmptySpaceIndex, dest_swapping_index);
                 this.mCurrentEmptySpaceIndex = dest_swapping_index;
+                this.mLogger.AddMove(this.mCurrentEmptySpaceIndex);
             }
         }
 
@@ -41,6 +97,7 @@ namespace _16ColorsPuzzle
             {
                 this.SwapTwoChips(this.mCurrentEmptySpaceIndex, dest_swapping_index);
                 this.mCurrentEmptySpaceIndex = dest_swapping_index;
+                this.mLogger.AddMove(this.mCurrentEmptySpaceIndex);
             }
         }
 
@@ -48,10 +105,11 @@ namespace _16ColorsPuzzle
         {
             int dest_swapping_index = this.mCurrentEmptySpaceIndex - 1;   //Item2 is the number of column
             //do the swapping only when the dest_swapping_index is within range.
-            if (0 <= dest_swapping_index)
+            if (0 <= dest_swapping_index && 0 != ((this.mCurrentEmptySpaceIndex) % this.mRowColumnConfig.Item2))
             {
                 this.SwapTwoChips(this.mCurrentEmptySpaceIndex, dest_swapping_index);
                 this.mCurrentEmptySpaceIndex = dest_swapping_index;
+                this.mLogger.AddMove(this.mCurrentEmptySpaceIndex);
             }
         }
 
@@ -59,10 +117,11 @@ namespace _16ColorsPuzzle
         {
             int dest_swapping_index = this.mCurrentEmptySpaceIndex + 1;   //Item2 is the number of column
             //do the swapping only when the dest_swapping_index is within range.
-            if (this.mChipsList.Count > dest_swapping_index)
+            if (this.mChipsList.Count > dest_swapping_index && (this.mRowColumnConfig.Item2 - 1) != ((this.mCurrentEmptySpaceIndex) % this.mRowColumnConfig.Item2))
             {
                 this.SwapTwoChips(this.mCurrentEmptySpaceIndex, dest_swapping_index);
                 this.mCurrentEmptySpaceIndex = dest_swapping_index;
+                this.mLogger.AddMove(this.mCurrentEmptySpaceIndex);
             }
         }
 
@@ -109,6 +168,21 @@ namespace _16ColorsPuzzle
                     g.DrawLine(pen, new Point(rtg.X + rtg.Width, rtg.Y), new Point(rtg.X, rtg.Y + rtg.Height));
                 }
             }
+        }
+
+        public bool ReachGoal()
+        {
+            int number_of_row = this.mRowColumnConfig.Item1;
+            int number_of_column = this.mRowColumnConfig.Item2;
+            int offset_first_row_last_row = (number_of_row - 1) * number_of_column;
+            for (int i = 0; i < number_of_column; i++)
+            {
+                if(this.mChipsList[i] != this.mChipsList[i+offset_first_row_last_row])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
