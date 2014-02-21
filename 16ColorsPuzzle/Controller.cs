9b3f3230f87d810e35c098c35b0177f188cd8cdf
Lin Chen <qkthomas@gmail.com>
2014-02-21 11:10:06 -0500

    added 4 movement methods and drawing method

--- 16ColorsPuzzle/Controller.cs
+++ 16ColorsPuzzle/Controller.cs
@@ -9,20 +9,61 @@ namespace _16ColorsPuzzle
 {
     class Controller
     {
+        private Tuple<int, int> mRowColumnConfig;
         private ChipsList mChipsList;
         private int mCurrentEmptySpaceIndex;
 
         enum Directions { up, down, left, right }
 
-        public Controller()
+        public Controller(Tuple<int, int> row_column_config)
         {
+            this.mRowColumnConfig = row_column_config;
             this.mChipsList = new ChipsList();
         }
 
-        //better to use delegate
-        public void MoveEmptySpaceInDirection(Directions direct)
+        //better to use delegate, and also need refactoring
+        public void MoveEmptySpaceUp()
         {
+            int dest_swapping_index = this.mCurrentEmptySpaceIndex - this.mRowColumnConfig.Item2;   //Item2 is the number of column
+            //do the swapping only when the dest_swapping_index is within range.
+            if (0 <= dest_swapping_index)
+            {
+                this.SwapTwoChips(this.mCurrentEmptySpaceIndex, dest_swapping_index);
+                this.mCurrentEmptySpaceIndex = dest_swapping_index;
+            }
+        }
 
+        public void MoveEmptySpaceDown()
+        {
+            int dest_swapping_index = this.mCurrentEmptySpaceIndex + this.mRowColumnConfig.Item2;   //Item2 is the number of column
+            //do the swapping only when the dest_swapping_index is within range.
+            if (this.mChipsList.Count > dest_swapping_index)
+            {
+                this.SwapTwoChips(this.mCurrentEmptySpaceIndex, dest_swapping_index);
+                this.mCurrentEmptySpaceIndex = dest_swapping_index;
+            }
+        }
+
+        public void MoveEmptySpaceLeft()
+        {
+            int dest_swapping_index = this.mCurrentEmptySpaceIndex - 1;   //Item2 is the number of column
+            //do the swapping only when the dest_swapping_index is within range.
+            if (0 <= dest_swapping_index)
+            {
+                this.SwapTwoChips(this.mCurrentEmptySpaceIndex, dest_swapping_index);
+                this.mCurrentEmptySpaceIndex = dest_swapping_index;
+            }
+        }
+
+        public void MoveEmptySpaceRight()
+        {
+            int dest_swapping_index = this.mCurrentEmptySpaceIndex + 1;   //Item2 is the number of column
+            //do the swapping only when the dest_swapping_index is within range.
+            if (this.mChipsList.Count > dest_swapping_index)
+            {
+                this.SwapTwoChips(this.mCurrentEmptySpaceIndex, dest_swapping_index);
+                this.mCurrentEmptySpaceIndex = dest_swapping_index;
+            }
         }
 
         public void SwapTwoChips(int index1, int index2)
@@ -36,10 +77,10 @@ namespace _16ColorsPuzzle
         public void ReadConfiguration(string filename)
         {
             string[] inputed_str = PzzConfigFileReader.ReadAsArray(filename);
-            this.mChipsList = PzzConfigFileReader.BuildChipsConfiguration(Tuple.Create<int, int>(3, 5), inputed_str);
+            this.mChipsList = PzzConfigFileReader.BuildChipsConfiguration(this.mRowColumnConfig, inputed_str);
             for (int i = 0; i < this.mChipsList.Count; i++ )
             {
-                if (Color.Transparent == this.mChipsList[i].chip_color)
+                if (Color.Transparent == this.mChipsList[i].mChipColor)
                 {
                     this.mCurrentEmptySpaceIndex = i;
                 }
@@ -49,9 +90,24 @@ namespace _16ColorsPuzzle
 
         public void DrawConfiguration(Graphics g, Size s)
         {
-            foreach (Chip cp in mChipsList)
+            for (int i = 0; i < this.mChipsList.Count; i++ )
             {
-                cp.DrawMyself(g, s);
+                int position = i;   //positio = index.
+                Brush brush = new SolidBrush(this.mChipsList[i].mChipColor);
+                Pen pen = new Pen(Color.Black);
+                //attain chip drawing size according to the size of the Panel.
+                int column_number = position % this.mRowColumnConfig.Item2;
+                int row_number = position / this.mRowColumnConfig.Item2;
+                Point rtg_upperleft_point = new Point(s.Width / this.mRowColumnConfig.Item2 * column_number, s.Height / this.mRowColumnConfig.Item1 * row_number);
+                Size rtg_size = new Size(s.Width / this.mRowColumnConfig.Item2, s.Height / this.mRowColumnConfig.Item1);
+                Rectangle rtg = new Rectangle(rtg_upperleft_point, rtg_size);
+                g.FillRectangle(brush, rtg);
+                g.DrawRectangle(pen, rtg);
+                if (Color.Transparent == this.mChipsList[i].mChipColor)
+                {
+                    g.DrawLine(pen, new Point(rtg.X, rtg.Y), new Point(rtg.X + rtg.Width, rtg.Y + rtg.Height));
+                    g.DrawLine(pen, new Point(rtg.X + rtg.Width, rtg.Y), new Point(rtg.X, rtg.Y + rtg.Height));
+                }
             }
         }
     }
