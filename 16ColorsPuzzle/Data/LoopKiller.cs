37cce7aeb04c325f6a5f40966dbf90b35790eede
Lin Chen <qkthomas@gmail.com>
2014-03-11 14:18:30 -0400

    Added isInCloseList(), AddToOpenList(), AddToCloseList() methods

--- 16ColorsPuzzle/Data/LoopKiller.cs
+++ 16ColorsPuzzle/Data/LoopKiller.cs
@@ -10,5 +10,27 @@ namespace _16ColorsPuzzle.Data
     {
         private List<State> mOpenList = new List<State>();
         private List<State> mCloseList = new List<State>();
+
+        public bool isInCloseList(State IN_state)
+        {
+            foreach(State s in this.mCloseList)
+            {
+                if(s == IN_state)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void AddToOpenList(State IN_state)
+        {
+            this.mOpenList.Add(IN_state);
+        }
+
+        public void AddToCloseList(State IN_state)
+        {
+            this.mCloseList.Add(IN_state);
+        }
     }
 }
