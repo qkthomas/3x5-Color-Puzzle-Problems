044c35c4552dd1d962ba5b258a03926d964225ec
Lin Chen <qkthomas@gmail.com>
2014-03-15 16:35:26 -0400

    State.cs: better format the codes; hided ReachGoal() method implementing IComparable<State>

--- 16ColorsPuzzle/Data/State.cs
+++ 16ColorsPuzzle/Data/State.cs
@@ -8,7 +8,7 @@ using System.Drawing;
 
 namespace _16ColorsPuzzle.Data
 {
-    class State
+    class State : IComparable<State>
     {
         #region Static Fields
         public static readonly Tuple<int, int> smRowColumnConfig = Tuple.Create<int, int>(3, 5);
@@ -40,15 +40,59 @@ namespace _16ColorsPuzzle.Data
         }
 	#endregion
 
+        #region Iplementation of IComparable
+        int IComparable<State>.CompareTo(State other_state)
+        {
+            if(this.HeuristicValue > other_state.HeuristicValue)
+            {
+                return 0x100;
+            }
+            else if(this.HeuristicValue < other_state.HeuristicValue)
+            {
+                return -0x100;
+            }
+            else
+            {
+                return 0;
+            }
+        } 
+        #endregion
+
+
+        #region non-static fields
         private ChipsList mChipsList = new ChipsList();
         private int mCurrentEmptySpaceIndex;
         private char mDestOfPreviousMove = '_';
         private bool mGoal = false;
+        private int HeuristicValue = int.MaxValue;
+        #endregion
 
+        #region Object creation, clone
         public static State CreateNewStateFromChipsList(ChipsList lst_chips)
         {
             State new_state = new State(lst_chips);
+            new_state.StateEvaluation();
             new_state.ReachGoal();
+            return new_state;
+        }
+
+        public State CreatNewStateFromMoving(IMover<State> mover)
+        {
+            State new_state = mover.Move(this);
+            new_state.StateEvaluation();
+            new_state.ReachGoal();
+            return new_state;
+        }
+
+        //maybe it is not a good idea.
+        public State NewShallowClone()
+        {
+            State new_state = this.MemberwiseClone() as State;
+            new_state.mChipsList = new ChipsList();
+            foreach (Chip c in this.mChipsList)
+            {
+                new_state.mChipsList.Add(c);
+            }
             return new_state;
         }
 
@@ -56,7 +100,7 @@ namespace _16ColorsPuzzle.Data
         {
             this.mChipsList = lst_chips;
             int how_many_empty_space = 0;
-            for (int i = 0; i < this.mChipsList.Count; i++ )
+            for (int i = 0; i < this.mChipsList.Count; i++)
             {
                 if (this.mChipsList[i].mChipColor == Color.Transparent)
                 {
@@ -72,8 +116,10 @@ namespace _16ColorsPuzzle.Data
             {
                 throw new Exception("There is no empty space in the configuration!!");
             }
-        }
+        } 
+        #endregion
 
+        #region Properties
         public int CurrentEmptySpaceIndex
         {
             get { return this.mCurrentEmptySpaceIndex; }
@@ -89,14 +135,34 @@ namespace _16ColorsPuzzle.Data
                 }
             }
         }
-        
+
         public bool Goal
         {
             get { return this.mGoal; }
             set { this.mGoal = value; }
         }
 
-        public void ReachGoal()
+        //may lead to some problems
+        public ChipsList InnerChipsList
+        {
+            get { return this.mChipsList; }
+        }
+
+        public char DestinationOfPreviousMove
+        {
+            get { return this.mDestOfPreviousMove; }
+            set { this.mDestOfPreviousMove = value; }
+        } 
+        #endregion
+
+        #region evaluation and goal checking
+        //the smaller the better
+        private void StateEvaluation()
+        {
+
+        }
+
+        private void ReachGoal()
         {
             int number_of_row = State.smRowColumnConfig.Item1;
             int number_of_column = State.smRowColumnConfig.Item2;
@@ -113,31 +179,7 @@ namespace _16ColorsPuzzle.Data
             this.mGoal = true;
             return;
             //return this.mGoal;
-        }
-
-        //maybe it is not a good idea.
-        public State NewShallowClone()
-        {
-            State new_state = this.MemberwiseClone() as State;
-            new_state.mChipsList = new ChipsList();
-            foreach(Chip c in this.mChipsList)
-            {
-                new_state.mChipsList.Add(c);
-            }
-            return new_state;
-        }
-
-        //may lead to some problems
-        public ChipsList InnerChipsList
-        {
-            get { return this.mChipsList; }
-        }
-
-        public char DestinationOfPreviousMove
-        {
-            get { return this.mDestOfPreviousMove; }
-            set { this.mDestOfPreviousMove = value; }
-        }
-
+        } 
+        #endregion
     }
 }
