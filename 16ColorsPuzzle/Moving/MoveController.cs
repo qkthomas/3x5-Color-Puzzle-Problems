5daa5528acada2764bb1533238b9ee744cc04753
Lin Chen <qkthomas@gmail.com>
2014-03-19 00:07:00 -0400

    MoveController.cs: added OneDIndexToTwoDIndex() method and GetManhattanDistance() method

--- 16ColorsPuzzle/Moving/MoveController.cs
+++ 16ColorsPuzzle/Moving/MoveController.cs
@@ -9,6 +9,21 @@ namespace _16ColorsPuzzle.Moving
 {
     static class MoveController
     {
+        public static Tuple<int, int> OneDIndexToTwoDIndex(Tuple<int, int> rcconfig, int oned_index)
+        {
+            int how_many_row = rcconfig.Item1;
+            int how_many_column = rcconfig.Item2;
+            int x = oned_index / how_many_column;
+            int y = oned_index % how_many_column;
+            return Tuple.Create<int, int>(x, y);
+        }
+
+        public static int GetManhattanDistance(Tuple<int, int> index_a, Tuple<int, int> index_b)
+        {
+            int distance = Math.Abs(index_a.Item1 - index_b.Item1) + Math.Abs(index_a.Item2 - index_b.Item2);
+            return distance;
+        }
+
         //better to use delegate, and also need refactoring
         public static void MoveEmptySpaceUp(State s)
         {
