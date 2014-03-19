edd8c7dbeef779c664cfcb38647e9166ceba9d83
Lin Chen <qkthomas@gmail.com>
2014-03-19 13:56:14 -0400

    HFunctions.cs: set GetManhattanDistance() and OneDIndexToTwoDIndex() methods to private

--- 16ColorsPuzzle/Heuristic/HFunctions.cs
+++ 16ColorsPuzzle/Heuristic/HFunctions.cs
@@ -8,7 +8,7 @@ namespace _16ColorsPuzzle.Heuristic
 {
     class HFunctions
     {
-        public static Tuple<int, int> OneDIndexToTwoDIndex(Tuple<int, int> rcconfig, int oned_index)
+        private static Tuple<int, int> OneDIndexToTwoDIndex(Tuple<int, int> rcconfig, int oned_index)
         {
             int how_many_row = rcconfig.Item1;
             int how_many_column = rcconfig.Item2;
@@ -17,7 +17,7 @@ namespace _16ColorsPuzzle.Heuristic
             return Tuple.Create<int, int>(x, y);
         }
 
-        public static int GetManhattanDistance(Tuple<int, int> index_a, Tuple<int, int> index_b)
+        private static int GetManhattanDistance(Tuple<int, int> index_a, Tuple<int, int> index_b)
         {
             int distance = Math.Abs(index_a.Item1 - index_b.Item1) + Math.Abs(index_a.Item2 - index_b.Item2);
             return distance;
